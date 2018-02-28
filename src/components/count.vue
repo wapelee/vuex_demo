121eff37a0be7fdf495ac6f0f45fa5591f83a3f3
李伟鹏 <279467973@qq.com>
2018-02-28 11:32:45 +0800

    vuex_demo

---
+++ src/components/count.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    {{count}}
+    <hr/>
+    <h3>{{$store.state.a.count}}</h3>
+    <button @click="add(10)">+</button>
+    <button @click="reduce">-</button>
+    <button @click="addAction">+</button>
+    <button @click="reduceAction">-</button>
+  </div>
+
+</template>
+<script>
+import store from "@/vuex/store";
+import { mapState, mapMutations, mapGetters, mapActions } from "vuex";
+export default {
+    data() {
+        return {
+            msg: "Hello Vuex"
+        };
+    },
+    store,
+    computed: {
+        ...mapGetters(["count"])
+    },
+    methods: {
+        ...mapMutations(["add", "reduce"]),
+        ...mapActions(["addAction", "reduceAction"])
+    }
+};
+</script>
---
+++ src/vuex/store.js
@@ -0,0 +1,39 @@
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
+
+const state = {
+  count: 1
+};
+const mutations = {
+  add(state, n) {
+    state.count += n;
+  },
+  reduce(state) {
+    state.count--;
+  }
+};
+const getters = {
+  count: function(state) {
+    return (state.count += 100);
+  }
+};
+const actions = {
+  addAction(context) {
+
+    setTimeout(() => {
+      context.commit("add", 10);
+    }, 3000);
+    console.log("我比add提前执行");
+  },
+  reduceAction({ commit }) {
+    commit("reduce");
+  }
+};
+
+const moduleA={
+  state,mutations,getters,actions
+}
+export default new Vuex.Store({
+  modules:{a:moduleA}
+});
